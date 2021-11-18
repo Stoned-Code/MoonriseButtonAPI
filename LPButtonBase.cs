aca446b34e72ad58bef64c67b0423b4d5c597ff9
Stoned-Code <animecomcommunity@gmail.com>
2021-11-18 14:27:59 -0800

    Initial Commit

---
+++ LPButtonBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MoonriseV2Mod.API.MoonriseButtonAPI
+{
+    public class ButtonBase
+    {
+        private GameObject buttonObject;
+        private Color backgroundColor;
+        private Color textColor;
+
+        public void Destroy()
+        {
+            UnityEngine.Object.Destroy(buttonObject);
+        }
+
+        public virtual void SetBackgroundColor()
+    }
+}
---
+++ MoonriseButtonApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonriseButtonAPI
+{
+    public static class MoonriseButtonApi
+    {
+        public static string modIdentifier = "Moonrise";
+    }
+}
